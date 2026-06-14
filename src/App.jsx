dd1198bb5eb403fd47a77b54a3e4bec1c1f15b3a
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-06-14 18:49:20 +0300

    src klasoru icindeki app.jsx component icinde container div'i icindeki main alaninda gorevler listesi manuel olarak olusturuldu, listenin altina toplam gorev listesi bilgisi ve tum gorevleri sil yazısı eklendi, app.css dosyasinda css duzenlemesi yapildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import LightThemeMobileBackground from './assets/img/light-theme-mobile-background.png'
 import SwitchingLightThemeDarkTheme from './assets/img/switching-light-theme-dark-theme.png'
+import TodoDelete from './assets/img/icon-todo-delete.png'
 import './App.css'
 
 export default function App() {
@@ -26,7 +27,34 @@ export default function App() {
           <div className="todos">
             <div className="todo">
               <input type="radio" name="todo" value="todo" id="todo-radio" />
-              <label htmlFor='todo-radio'>Görev</label>
+              <label htmlFor='todo-radio'>Complete online JavaScript course</label>
+              <img src={TodoDelete} alt="" />
+            </div>
+            <div className="todo">
+              <input type="radio" name="todo" value="todo" id="todo-radio" />
+              <label htmlFor='todo-radio'>Jog around the park 3x</label>
+              <img src={TodoDelete} alt="" />
+            </div>
+            <div className="todo">
+              <input type="radio" name="todo" value="todo" id="todo-radio" />
+              <label htmlFor='todo-radio'>10 minutes meditation</label>
+              <img src={TodoDelete} alt="" />
+            </div>
+            <div className="todo">
+              <input type="radio" name="todo" value="todo" id="todo-radio" />
+              <label htmlFor='todo-radio'>Read for 1 hour</label>
+              <img src={TodoDelete} alt="" />
+            </div>
+            <div className="todo">
+              <input type="radio" name="todo" value="todo" id="todo-radio" />
+              <label htmlFor='todo-radio'>Pick up groceries</label>
+              <img src={TodoDelete} alt="" />
+            </div>
+            <div className="todo">
+              <div className="selects">
+                <h6>5 items left</h6>
+                <h6>Clear Completed</h6>
+              </div>
             </div>
           </div>
         </main>
